708a337df8296cd6b2c2586ba02c9583c24564b5
urara <osamu.oura@gmail.com>
2015-10-20 01:20:19 +0900

    add action dispatcher constants

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,8 +1,12 @@
 var React = require('react');
+var AppActions = require('../actions/app-actions');
 
 var App = React.createClass({
+    handler: function(){
+        AppActions.addItem('this is an item')
+    },
     render: function () {
-        return <h1>Hello My App</h1>
+        return <h1 onClick={this.handler}>Hello My App</h1>
     }
 });
 
